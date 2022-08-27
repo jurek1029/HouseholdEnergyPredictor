dc12aaaf2fbb9f948e2d6fc2049566a8b6e18973
jurek1029 <jurek1029@gmail.com>
2022-08-27 22:44:48 +0200

    TensorFlowLitePython test

--- TensorFlowLite/src/main.cpp
+++ TensorFlowLite/src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
+//LED BUILT_IN is GPIO 33 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(33, OUTPUT); // Set the pin as output
 }
 
+// Remember that the pin work with inverted logic
+// LOW to Turn on and HIGH to turn off
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(33, LOW); //Turn on
+  delay (1000); //Wait 1 sec
+  digitalWrite(33, HIGH); //Turn off
+  delay (1000); //Wait 1 sec
 }
